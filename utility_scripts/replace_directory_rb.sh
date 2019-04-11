c33cd567ae7253746e139331016374a1ed79b424
wadedismukes <wade.dismukes@gmail.com>
2019-04-11 14:05:42 -0500

    added utility script to replace

---
+++ utility_scripts/replace_directory_rb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd rev-scripts/
+
+all=$(ls)
+for sub in $all
+do
+    cd $sub
+    find *.Rev | xargs sed -i "s/data\//\/work\/LAS\/phylo-lab\/wade\/dec-cophylo\/data/g"
+    cd ../
+done

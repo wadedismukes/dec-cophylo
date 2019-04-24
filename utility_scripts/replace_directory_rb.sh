48c5f0bbf36d52ae50a60ea9b6cee2933bfb26b1
wadedismukes <wade.dismukes@gmail.com>
2019-04-24 12:57:31 -0500

    added some shell scripts to change rev scripts

--- utility_scripts/replace_directory_rb.sh
+++ utility_scripts/replace_directory_rb.sh
@@ -6,6 +6,6 @@ all=$(ls)
 for sub in $all
 do
     cd $sub
-    find *.Rev | xargs sed -i "s/data\//\/work\/LAS\/phylo-lab\/wade\/dec-cophylo\/data/g"
+    find *.Rev | xargs sed -i "s/data\//\/work\/LAS\/phylo-lab\/wade\/dec-cophylo\/data\//g"
     cd ../
 done
---
+++ utility_scripts/run_underprior_0.05.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copy/paste this job script into a text file and submit with the command:
+#    sbatch thefilename
+# job standard output will go to the file slurm-%j.out (where %j is the job ID)
+
+#SBATCH --time=14-00:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=16   # 16 processor core(s) per node 
+#SBATCH --mem=32G   # maximum memory per node
+#SBATCH --job-name="run-rev-all"
+#SBATCH --mail-user=waded@iastate.edu   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load revbayes
+
+rb rev-scripts/hsrate_0.05/run_epoch_hsrate_0.05_0.Rev 
